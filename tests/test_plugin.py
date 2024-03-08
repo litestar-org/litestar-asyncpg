7c9d4fb0aab88fab07115599737901751c413509
euri10 <benoit.barthelet@gmail.com>
2024-03-08 11:23:12 +0100

    add test for not default key

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -30,3 +30,23 @@ async def test_lifespan(postgres_service: None, docker_ip: str) -> None:
     with create_test_client(route_handlers=[health_check], plugins=[asyncpg], lifespan=[partial(lifespan)]) as client:
         response = client.get("/")
         assert response.status_code == 200
+
+
+async def test_not_default_key(postgres_service: None, docker_ip: str) -> None:
+    @get("/")
+    async def health_check(my_connection: Connection) -> float:
+        """Check database available and returns random number."""
+        r = await my_connection.fetch("select random()")
+        return r[0]["random"]  # type: ignore
+
+    @asynccontextmanager
+    async def lifespan(_app: Litestar) -> AsyncGenerator[None, Any]:
+        print(1)  # noqa: T201
+        yield
+        print(2)  # noqa: T201
+
+    asyncpg_config = AsyncpgConfig(pool_config=PoolConfig(dsn=f"postgresql://app:app@{docker_ip}:5423/app"), connection_dependency_key="my_connection")
+    asyncpg = AsyncpgPlugin(config=asyncpg_config)
+    with create_test_client(route_handlers=[health_check], plugins=[asyncpg], lifespan=[partial(lifespan)]) as client:
+        response = client.get("/")
+        assert response.status_code == 200
